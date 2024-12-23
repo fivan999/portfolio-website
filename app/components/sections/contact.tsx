c8d4f9ea1c39582f31bca7c9ac6425c0ad731e5b
bebra_dev <fivan999@yandex.ru>
2024-12-23 16:55:13 +0300

    chore: change contacts and description

--- app/components/sections/contact.tsx
+++ app/components/sections/contact.tsx
@@ -8,11 +8,11 @@ export default function ContactSection() {
       <div className="absolute top-0 bottom-0 md:translate-y-0 -translate-y-12 text-4xl z-10 flex items-center justify-center">
         <h4 className="select-none">Есть предложения?</h4>
       </div>
-      <div title="Отправить мне письмо" id="contact" className="bg-[#33498C] z-0 flex justify-center items-center absolute w-[582px] h-[582px] top-0 bottom-0 left-0 right-0 -translate-y-1/3 mx-auto rounded-full">
-        <Link className="select-none" href="mailto:contact@bebradev.ru"><p className="text-[16rem] hidden lg:block antialiased select-none mb-12">@</p></Link>
+      <div title="Написать мне" id="contact" className="bg-[#33498C] z-0 flex justify-center items-center absolute w-[582px] h-[582px] top-0 bottom-0 left-0 right-0 -translate-y-1/3 mx-auto rounded-full">
+        <p className="text-[16rem] hidden lg:block antialiased select-none mb-12">@</p>
       </div>
-      <div title="Отправить мне письмо" className="absolute top-0 md:translate-y-0 translate-y-12 bottom-0 right-[9999px] text-4xl z-10 flex items-center justify-center">
-        <Link href={`mailto:${CONTACT_URL}`}>
+      <div title="Написать мне" className="absolute top-0 md:translate-y-0 translate-y-12 bottom-0 right-[9999px] text-4xl z-10 flex items-center justify-center">
+        <Link href={CONTACT_URL}>
           <h4 className="hover:text-teal-300 transition-all duration-300 hover:scale-110">{CONTACT_URL}</h4>
         </Link>
       </div>
